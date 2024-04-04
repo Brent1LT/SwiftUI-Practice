fe51de449799f6d534e5a5624176759af6d0dd21
Brent Bumann <brent.bumann@aol.com>
2024-04-04 13:40:30 -0700

    Initial commit

---
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Brent Bumann on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
